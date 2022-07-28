b21b0ba13b10f188f7269b904b2c7e4874072293
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-28 11:27:44 -0400

    function to get the file path

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import http from "http"
-import fs from "fs/promises"
-import path from "path"
+import http from "http";
+import fs from "fs/promises";
+import path from "path";
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" })
-  res.write("Hello World")
-  res.end()
-})
+  if (req.url) {
+    getFileData(req.url);
+  }
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("Hello World");
+  res.end();
+});
 
 server.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`)
-})
+  console.log(`Server running at port ${PORT}`);
+});
+
+const getFileData = async (url: string) => {
+  const filePath = getFilePath(url); // function to get the filePath
+};
+
+const getFilePath = (url: string) => {
+  const fileName = url === "/" ? "index" : url;
+  const filePath = path.join(__dirname, "/public", `${fileName}.html`);
+  return filePath;
+};
