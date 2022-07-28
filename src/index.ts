9952bc8bd7084a53efb7d622cd5dc44dd9aaab63
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-28 04:40:01 -0400

    created basic http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+// import fs from "fs"
+import http from "http"
+
+const port = process.env.PORT || 8080
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/html")
+  res.end("<h1>Hello, World!</h1>")
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
