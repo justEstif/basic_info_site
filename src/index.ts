15f809a69605d4dff9a04be5b2136678648407e4
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-28 11:51:07 -0400

    function that receives url and returns the response

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,13 @@ server.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
 
-const getFileData = async (url: string) => {
-  const filePath = getFilePath(url); // function to get the filePath
+const getResponse = async (url: string) => {
+  const filePath = getFilePath(url);
+  const { fileData, statusCode } = await getFileData(filePath);
+  return {
+    fileData,
+    statusCode,
+  };
 };
 
 const getFilePath = (url: string) => {
