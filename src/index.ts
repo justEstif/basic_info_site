7b91c6ffd48729aecef15ac77603fa413d564bbd
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-02 15:48:44 -0400

    minor styling additions

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
-import express, { Express, Request, Response } from "express";
-import path from "path";
+import express, { Express, Request, Response } from "express"
+import path from "path"
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
-//
+const app: Express = express()
+const port = process.env.PORT || 3000
+
 const pages = {
   index: path.normalize("./public/index.html"),
   about: path.normalize("./public/about.html"),
   contact: path.normalize("./public/contact-me.html"),
   error: path.normalize("./public/404.html"),
-};
+}
 
 app.get("/", (_: Request, res: Response) => {
-  res.sendFile(pages.index, { root: path.join(__dirname, "../") });
-});
+  res.sendFile(pages.index, { root: path.join(__dirname, "../") })
+})
 
 app.get("/about", (_: Request, res: Response) => {
-  res.sendFile(pages.about, { root: path.join(__dirname, "../") });
-});
-//
+  res.sendFile(pages.about, { root: path.join(__dirname, "../") })
+})
+
 app.get("/contact-me", (_: Request, res: Response) => {
-  res.sendFile(pages.contact, { root: path.join(__dirname, "../") });
-});
-//
+  res.sendFile(pages.contact, { root: path.join(__dirname, "../") })
+})
+
 app.get("/*", (_: Request, res: Response) => {
-  res.sendFile(pages.error, { root: path.join(__dirname, "../") });
-});
+  res.sendFile(pages.error, { root: path.join(__dirname, "../") })
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
